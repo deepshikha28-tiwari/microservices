801d1af8a2cdb0d1aeb4ecb9a115418344a35cb8
deepshikha28-tiwari <deepshikha.t28@gmail.com>
2026-07-09 01:12:51 +0530

    third commit

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,11 @@ git pull origin main
 
 docker compose down
 
-<<<<<<< HEAD
 docker compose build
 
 docker compose up -d 
-=======
-docker compose down
 
-docker compose up 
->>>>>>> fc699ae3e18734383e822e70d6ae1fd61d0fac47
+
 
 
 
